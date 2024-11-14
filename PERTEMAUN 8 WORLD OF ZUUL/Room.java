615ee712fabc9848641da20c5121c74badc62551
Mazaya Khairana <162776393+niariswari@users.noreply.github.com>
2024-11-14 09:10:02 +0700

    Create Room.java

---
+++ PERTEMAUN 8 WORLD OF ZUUL/Room.java
@@ -0,0 +1,29 @@
+public class Room {
+    private String description;
+    private HashMap<String, Room> exits;
+
+    public Room(String description) {
+        this.description = description;
+        exits = new HashMap<>();
+    }
+
+    public void setExit(String direction, Room neighbor) {
+        exits.put(direction, neighbor);
+    }
+
+    public String getLongDescription() {
+        return "You are " + description + ".\n" + getExitString();
+    }
+
+    private String getExitString() {
+        StringBuilder returnString = new StringBuilder("Exits:");
+        for (String exit : exits.keySet()) {
+            returnString.append(" ").append(exit);
+        }
+        return returnString.toString();
+    }
+
+    public Room getExit(String direction) {
+        return exits.get(direction);
+    }
+}
