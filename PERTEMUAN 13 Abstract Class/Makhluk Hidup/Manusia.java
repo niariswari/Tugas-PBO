0687bf9c5e19e8e7dff1b9c52450074557052bb7
niariswari <mazayakhrn@example.com>
2024-12-20 14:03:50 +0700

    make folder

---
+++ PERTEMUAN 13 Abstract Class/Makhluk Hidup/Manusia.java
@@ -0,0 +1,12 @@
+public class Manusia extends MakhlukHidup {
+    @Override
+    public void bernapas() {
+        System.out.println("Manusia bernapas menggunakan paru-paru.");
+    }
+
+    @Override
+    public void berkembangbiak() {
+        System.out.println("Manusia berkembang biak dengan melahirkan.");
+    }
+}
+
