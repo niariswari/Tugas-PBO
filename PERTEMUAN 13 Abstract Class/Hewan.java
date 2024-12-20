5354cbb886d8c8509223ce3b9721e414e0816d77
Mazaya Khairana <162776393+niariswari@users.noreply.github.com>
2024-12-20 13:55:08 +0700

    Add files via upload

---
+++ PERTEMUAN 13 Abstract Class/Hewan.java
@@ -0,0 +1,12 @@
+public class Hewan extends MakhlukHidup {
+    @Override
+    public void bernapas() {
+        System.out.println("Hewan bernapas menggunakan paru-paru atau insang.");
+    }
+
+    @Override
+    public void berkembangbiak() {
+        System.out.println("Hewan berkembang biak secara ovipar (bertelur), vivipar (melahirkan), atau ovovivipar (kombinasi keduanya).");
+    }
+}
+
---
+++ PERTEMUAN 13 Abstract Class/MakhlukHidup.java
@@ -0,0 +1,9 @@
+public abstract class MakhlukHidup {
+    public abstract void bernapas();
+    public abstract void berkembangbiak();
+    
+    public void hidup() {
+        System.out.println("Makhluk hidup memiliki kemampuan untuk bertahan dan beradaptasi dengan lingkungannya.");
+    }
+}
+
